041a0af9857d57c34421b91a5322d46b047bc4f7
Subrat Rout <subratnrout@gmail.com>
2015-02-06 10:31:52 -0800

    Added tests for Initial Entries and New Feed Selection

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -83,6 +83,7 @@ $(function() {
     });
 
     /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function(){
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -91,10 +92,42 @@ $(function() {
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
 
-    /* TODO: Write a new test suite named "New Feed Selection"
+         beforeEach(function(done){
+            loadFeed(0, done);
+         });
 
+         it('contain at least a single .entry element within the .feed container', function(done){
+            var feedContainer = $('.feed').children();
+            expect(feedContainer).toBeDefined();
+            expect(feedContainer).not.toBe(0);
+            expect(feedContainer.length).toBeGreaterThan(0);
+            done();
+         });
+    });
+
+    /* TODO: Write a new test suite named "New Feed Selection" */
+    describe('New Feed Selection', function(){
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+         var feedTexts = $('.feed').find('h2').text();
+
+         beforeEach(function(done){
+            feedTexts;
+            loadFeed(1, done);
+         })
+
+         it('ensures that a new feed is loaded', function(done){
+            expect($('.feed').find('h2').text()).not.toBe(feedTexts);
+            done();
+         });
+
+         afterEach(function(done){
+            loadFeed(0, done);
+         });
+
+    });
+
+
 }());
